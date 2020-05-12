e4626cf7e75bea37649d46a11481cf7a7bfb1a24
Mr.Dai <38459993+ochimot@users.noreply.github.com>
2020-05-12 15:30:38 +0700

    update <,>

--- DoAnQint/Project/QInt_Comparison.cpp
+++ DoAnQint/Project/QInt_Comparison.cpp
@@ -14,25 +14,30 @@ bool QInt::operator == (QInt const &a)
 	return true;
 }
 
-
+// Operator =
+void QInt::operator = (QInt const& a)
+{
+	for (int i = 0; i < 4; i++)
+		this->bigInt[i] = a.bigInt[i];
+}
 // Operator <=
-bool QInt::operator <= (QInt const& a)
+bool QInt::operator <= (QInt const& b)
 {
 	//this -> a
 	// a dương b âm
-	if (GetSign(*this) == 0 && GetSign(a) == 1)
+	if (GetSign(*this) == 0 && GetSign(b) == 1)
 		return false;
 	// a âm b dương
-	if (GetSign(*this) == 1 && GetSign(a) == 0)
+	if (GetSign(*this) == 1 && GetSign(b) == 0)
 		return true;
 	// a b cùng dấu
 	// bit a dài hơn b
-	if (BitLength(*this) > BitLength(a))
+	if (BitLength(*this) > BitLength(b))
 	{
 		return false;
 	}
 	// bit b dài hơn a
-	if (BitLength(*this) < BitLength(a))
+	if (BitLength(*this) < BitLength(b))
 	{
 		return true;
 	}
@@ -41,7 +46,7 @@ bool QInt::operator <= (QInt const& a)
 	for (i; i <= 127; i++)
 	{
 		bool bita = GetBit(*this, i);
-		bool bitb = GetBit(a, i);
+		bool bitb = GetBit(b, i);
 
 		if (bita && bitb)
 			continue;
@@ -96,6 +101,90 @@ bool QInt::operator >= (QInt const& a)
 		{
 			if (bit2)
 				return false;
+		}
+	}
+}
+//operator >
+bool QInt::operator > (QInt const& b)
+{
+	//this -> a
+	// a dương b âm
+	if (GetSign(*this) == 0 && GetSign(b) == 1)
+		return true;
+	// a âm b dương
+	if (GetSign(*this) == 1 && GetSign(b) == 0)
+		return false;
+	// a b cùng dấu
+	// bit a dài hơn b
+	if (BitLength(*this) > BitLength(b))
+	{
+		return true;
+	}
+	// bit b dài hơn a
+	if (BitLength(*this) < BitLength(b))
+	{
+		return false;
+	}
+	// bit a b bằng nhau
+	int i = 127 - BitLength(*this) + 1;
+	for (i; i <= 127; i++)
+	{
+		bool bita = GetBit(*this, i);
+		bool bitb = GetBit(b, i);
+
+		if (bita && bitb)
+			return false;
+		if (bita)
+		{
+			if (!bitb)
+				return true;
+		}
+		if (!bita)
+		{
+			if (bitb)
+				return false;
+		}
+	}
+}
+//operator <
+bool QInt::operator < (QInt const& b)
+{
+	//this -> a
+	// a dương b âm
+	if (GetSign(*this) == 0 && GetSign(b) == 1)
+		return false;
+	// a âm b dương
+	if (GetSign(*this) == 1 && GetSign(b) == 0)
+		return true;
+	// a b cùng dấu
+	// bit a dài hơn b
+	if (BitLength(*this) > BitLength(b))
+	{
+		return false;
+	}
+	// bit b dài hơn a
+	if (BitLength(*this) < BitLength(b))
+	{
+		return true;
+	}
+	// bit a b bằng nhau
+	int i = 127 - BitLength(*this) + 1;
+	for (i; i <= 127; i++)
+	{
+		bool bita = GetBit(*this, i);
+		bool bitb = GetBit(b, i);
+
+		if (bita && bitb)
+			return false;
+		if (bita)
+		{
+			if (!bitb)
+				return false;
+		}
+		if (!bita)
+		{
+			if (bitb)
+				return true;
 		}
 	}
 }
