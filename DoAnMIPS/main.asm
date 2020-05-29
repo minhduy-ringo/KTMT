81173985653b959ad167d1338404a1592f806209
Võ Minh Duy <minhduy.ringo@gmail.com>
2020-05-29 18:46:30 +0700

    +Game GuessChar function done - wait for overall test *Reformat code typing

--- DoAnMIPS/main.asm
+++ DoAnMIPS/main.asm
@@ -1,146 +1,75 @@
 # Hang man game
 .data
-<<<<<<< HEAD
-tMenu:		.asciiz "===================Menu===================\n"
-tNewGame: 	.asciiz "1.New game\n"
-tPlayAgain: 	.asciiz "2.Play again\n"
-tScoreboard: 	.asciiz "3.Score board\n"
-tExit: 		.asciiz "4.Exit\n"
-tChoose: 	.asciiz "Choose:"
-FirstWrong:	.asciiz "\n______\n|/   |\n|\n|\n|\n|\n|\n"
-SecondWrong:	.asciiz "\n______\n|/   |\n|    O\n|\n|\n|\n|\n"
-ThirdWrong:	.asciiz "\n______\n|/   |\n|    O\n|    |\n|\n|\n|\n"
-FourthWrong:	.asciiz "\n______\n|/   |\n|    O\n|   /|\n|\n|\n|\n"
-FifthWrong:	.asciiz "\n______\n|/   |\n|    O\n|   /|\\\n|\n|\n|\n"
-SixthWrong:	.asciiz "\n______\n|/   |\n|    O\n|   /|\\\n|   /\n|\n|\n"
-SeventhWrong:	.asciiz "\n______\n|/   |\n|    O\n|   /|\\\n|   / \\\n|\n|\n"
-
-#
-error:		.word
-CheckWrong:	.word 7
-=======
-welcomeMsg:	.asciiz "Welcome to the hang man game\n\n"
+welcomeMsg:	.asciiz "Welcome to the hang man game\n"
 namePromt:	.asciiz "Please type in your player name: "
 menuMsg:	.asciiz "===================Menu===================\n"
 newGameMsg: 	.asciiz "1.Play\n"
 scoreboardMsg: 	.asciiz "2.Score board\n"
 exitMsg: 	.asciiz "3.Exit\n"
 chooseMsg: 	.asciiz "Choose: "
+breakBar:	.asciiz "==========================================\n"
 
 #
 playerName:	.space 32
 
->>>>>>> MIPS
 .text
 
 .globl main
 main:
-<<<<<<< HEAD
-	
-	# Display menu
-=======
->>>>>>> MIPS
-	li $v0, 4
-	la $a0, welcomeMsg
+	li 	$v0, 4
+	la 	$a0, welcomeMsg
 	syscall
 	# Player name promt
-	li $v0, 4
-	la $a0, namePromt
+	li 	$v0, 4
+	la 	$a0, namePromt
 	syscall
-	li $v0, 8
-	la $a0, playerName
-	la $a1, 32
+	li 	$v0, 8
+	la 	$a0, playerName
+	la 	$a1, 32
+	syscall
+	li 	$v0, 11
+	li 	$a0, 10
 	syscall
 
 	# Display menu
-	li $v0, 4
-	la $a0, menuMsg
+	li 	$v0, 4
+	la 	$a0, menuMsg
 	syscall
-	li $v0, 4
-	la $a0, newGameMsg
+	li 	$v0, 4
+	la 	$a0, newGameMsg
 	syscall
-	li $v0, 4
-	la $a0, scoreboardMsg 
+	li 	$v0, 4
+	la 	$a0, scoreboardMsg 
 	syscall
-	li $v0, 4
-	la $a0, exitMsg
+	li 	$v0, 4
+	la 	$a0, exitMsg
+	syscall
+	li 	$v0, 4
+	la 	$a0, breakBar
 	syscall
 
 	# Check user choosed input
 MenuLoop:
 	# Choose game option
-	li $v0, 4
-	la $a0, chooseMsg
+	li 	$v0, 4
+	la 	$a0, chooseMsg
 	syscall
-	li $v0, 5
+	li 	$v0, 5
 	syscall
 
-	beq $v0,1,GoGame
-	beq $v0,3,GoScoreBoard
-	beq $v0,4,GoExit
-	# test draw Hangman
-	beq $v0,5,DrawHangMan
-	j MenuLoop
-
+	beq 	$v0,1,GoGame
+	beq 	$v0,2,GoScoreBoard
+	beq 	$v0,3,GoExit
+	j 	MenuLoop
 #
-DrawHangMan:
-	lw $s0,CheckWrong
-
-	beq $s0,1,Fwrong
-	beq $s0,2,Swrong
-	beq $s0,3,Twrong
-	beq $s0,4,Fowrong
-	beq $s0,5,Fiwrong
-	beq $s0,6,Siwrong
-	beq $s0,7,Sewrong
-Fwrong:
-	li $v0,4
-	la $a0,FirstWrong
-	syscall
-	jal GoExit
-Swrong:
-	li $v0,4
-	la $a0,SecondWrong
-	syscall
-	jal GoExit
-Twrong:
-	li $v0,4
-	la $a0,ThirdWrong
-	syscall
-	jal GoExit
-Fowrong:
-	li $v0,4
-	la $a0,FourthWrong
-	syscall
-	jal GoExit
-Fiwrong:
-	li $v0,4
-	la $a0,FifthWrong
-	syscall
-	jal GoExit
-Siwrong:
-	li $v0,4
-	la $a0,SixthWrong
-	syscall
-	jal GoExit
-Sewrong:
-	li $v0,4
-	la $a0,SeventhWrong
-	syscall
-	jal GoExit
-	
 GoGame:
-<<<<<<< HEAD
-	#jal Game
-	jal GoExit
-=======
 	# $a0 = player name
-	la $a0, playerName
-	jal Game
->>>>>>> MIPS
+	la 	$a0, playerName
+	jal 	Game
 
 GoScoreBoard:
+	#jal 	ScoreBoard
 
 GoExit:
-	li $v0,10
+	li 	$v0,10
 	syscall
